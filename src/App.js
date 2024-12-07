7f8b58e4a39b066b600da8bde938b0961867dd9d
aghasi89 <albertpetoyan@mail.ru>
2024-12-07 23:53:20 +0400

    set 5 sec

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       preloader.style.display = "none";
       document.body.style.overflow = "auto"
 
-    }, 30000);
+    }, 5000);
   }, []);
   const lottieOptions = {
     loop: true,
